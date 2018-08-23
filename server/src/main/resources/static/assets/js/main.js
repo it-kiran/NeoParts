41e2e24e0c7053023d0f6ad3d4588141ff08a7be
alokpatel <alokpatel9824107152@gmail.com>
2018-08-23 18:12:26 -0400

    chnagez

---
+++ server/src/main/resources/static/assets/js/main.js
@@ -0,0 +1,43 @@
+/************* Banner Slider **************/
+$(document).ready(function() 
+{
+	debugger;
+  	// $("#banner-slider").owlCarousel(
+  	// {
+  	// 	items : 1,
+  	// 	nav: true,
+    //   loop: true,
+    //   	// smartSpeed : 2000,      
+    //   	autoplay: true,
+    //   	autoplaySpeed: 3000,
+    //     autoplayHoverPause: true
+  	// });
+  	// $(".product-slider").owlCarousel(
+  	// {
+  	// 	items : 6,
+    //   	loop: true,
+    //   	autoplay: true,
+    //   	autoplaySpeed: 3000,
+    //     autoplayHoverPause: false,
+    //     responsive:
+    //     {
+	//         0:{
+	//             items:1,
+    //           dots: false,
+	//         },
+	//         600:{
+	//             items:3,
+	//         },
+	//         1000:{
+	//             items:5,
+	//         },
+	//         1400:{
+	//             items:6,
+	//             loop:true
+	//         }
+	//     }
+  	// });
+});
+
+
+
